e4008b4fa8697772d23e7a24ca3de42d3207d30e
Alimardan <alimardan_akhmedov@hotmail.com>
2022-06-04 10:58:01 +0400

    remove versions-1

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'postgresql-djangoserver',
+        'NAME': 'postgres',
 
         'USER': 'turgayadmin',
 
